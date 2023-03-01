80a3393ab64859b1e4faf4c8eb6882d6dda730fa
KlimchukKS <klinchuk1996@yandex.ru>
2023-03-01 20:39:26 +0500

    Update stat_reader.cpp

--- transport-catalogue/stat_reader.cpp
+++ transport-catalogue/stat_reader.cpp
@@ -1,10 +1,8 @@
 #include "stat_reader.h"
 
-#include <iostream>
 #include <string>
 #include <iomanip>
 #include <stdexcept>
-#include <string_view>
 #include <algorithm>
 #include <unordered_set>
 
@@ -16,45 +14,45 @@ namespace transport_catalogue {
         using namespace std::string_view_literals;
 
         namespace detail {
-            void OutBusInfo(std::string_view bus, TransportCatalogue& transport_catalogue) {
+            void OutBusInfo(std::ostream& out, std::string_view bus, TransportCatalogue& transport_catalogue) {
                 try {
                     auto [bus_name,
                             stops_route,
                             unique_stops,
                             route_length,
                             curvature] = transport_catalogue.GetBusInfo(bus);
-                    std::cout << "Bus "s <<bus_name << ": "s
+                    out << "Bus "s <<bus_name << ": "s
                               << stops_route << " stops on route, "s
                               << unique_stops << " unique stops, "s
                               << route_length << " route length, "s
                               << std::setprecision(6)
                               << curvature << " curvature\n";
                 } catch (const std::logic_error& error) {
-                    std::cout << "Bus " << bus << ": not found\n";
+                    out << "Bus " << bus << ": not found\n";
                 }
             }
 
-            void OutStopInfo(std::string_view stop, TransportCatalogue& transport_catalogue) {
+            void OutStopInfo(std::ostream& out, std::string_view stop, TransportCatalogue& transport_catalogue) {
                 try {
                     std::unordered_set<std::string_view> buses = transport_catalogue.GetStopInfo(stop);
                     if (buses.empty()) {
-                        std::cout << "Stop " << stop << ": no buses\n";
+                        out << "Stop " << stop << ": no buses\n";
                     } else {
-                        std::cout << "Stop " << stop << ": buses ";
+                        out << "Stop " << stop << ": buses ";
                         std::vector<std::string_view> tmp(buses.size());
                         std::copy(buses.begin(), buses.end(), tmp.begin());
                         std::sort(tmp.begin(), tmp.end());
                         for (auto& bus : tmp) {
-                            std::cout << bus << " ";
+                            out << bus << " ";
                         }
-                        std::cout << "\n";
+                        out << "\n";
                     }
                 } catch (const std::logic_error& error) {
-                    std::cout << "Stop " << stop << ": not found\n";
+                    out << "Stop " << stop << ": not found\n";
                 }
             }
 
-            void ParseQuery(std::istream &input, TransportCatalogue& transport_catalogue) {
+            void ParseQuery(std::ostream& out, std::istream &input, TransportCatalogue& transport_catalogue) {
                 using namespace std::string_literals;
                 int number_requests;
                 input >> number_requests;
@@ -74,16 +72,16 @@ namespace transport_catalogue {
                     query.remove_prefix(std::min(query.find_first_not_of(" "), query.size()));
 
                     if (command == "Bus"sv) {
-                        OutBusInfo(query, transport_catalogue);
+                        OutBusInfo(out,query, transport_catalogue);
                     } else if (command == "Stop"sv) {
-                        OutStopInfo(query, transport_catalogue);
+                        OutStopInfo(out, query, transport_catalogue);
                     }
                 }
             }
         }
 
-        void ReadQueryAndOutInfo(std::istream &input, TransportCatalogue& transport_catalogue) {
-            detail::ParseQuery(input, transport_catalogue);
+        void ReadQueryAndOutInfo(std::ostream& out, std::istream &input, TransportCatalogue& transport_catalogue) {
+            detail::ParseQuery(out, input, transport_catalogue);
         }
     }
 }
