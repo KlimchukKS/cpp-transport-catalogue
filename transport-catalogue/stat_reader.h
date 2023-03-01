daeee3c7cd64cbb1d8ecbc6dea3ca38bf9ff7a22
KlimchukKS <klinchuk1996@yandex.ru>
2023-03-01 06:47:21 +0500

    Update stat_reader.h

--- transport-catalogue/stat_reader.h
+++ transport-catalogue/stat_reader.h
@@ -1,1 +1,22 @@
-// место для вашего кода
+#pragma once
+
+#include "transport_catalogue.h"
+
+//чтение запросов на вывод и сам вывод
+
+namespace transport_catalogue {
+
+    namespace stat_reader {
+
+        namespace detail {
+
+            void OutBusInfo(std::string_view bus, TransportCatalogue& transport_catalogue);
+
+            void OutStopInfo(std::string_view stop, TransportCatalogue& transport_catalogue);
+
+            void ParseQuery(std::istream &input, TransportCatalogue& transport_catalogue);
+        }
+
+        void ReadQueryAndOutInfo(std::istream &input, TransportCatalogue& transport_catalogue);
+    }
+}
