cbc52b4a5bee6d28e4919aa226f40e5f0071fc8e
KlimchukKS <klinchuk1996@yandex.ru>
2023-05-08 07:37:22 +0500

    Update transport_catalogue.h

--- transport-catalogue/transport_catalogue.h
+++ transport-catalogue/transport_catalogue.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "domain.h"
 #include "geo.h"
 
 #include <string>
@@ -8,29 +9,12 @@
 #include <vector>
 #include <unordered_map>
 #include <unordered_set>
-
-//класс транспортного справочника
+#include <optional>
 
 namespace transport_catalogue {
 
     class TransportCatalogue {
     private:
-        using BusInfo = std::tuple<std::string_view, int, int, int, double>;
-
-        struct Stop {
-            std::string stop_name;
-            Coordinates coordinates;
-        };
-
-        struct Bus {
-            std::string bus_name;
-            std::vector<Stop*> stops;
-            std::unordered_set<std::string_view> unique_stops;
-
-            int route_length = 0;
-            double geographic_distance = 0.;
-        };
-
         struct StopsDistanceHasher {
             size_t operator() (const std::pair<Stop*, Stop*> stops) const {
                 size_t h_x = d_hasher_(stops.first);
@@ -47,22 +31,27 @@ namespace transport_catalogue {
         std::deque<Bus> buses_;
         std::unordered_map<std::string_view, Stop*> stopname_to_stop_;
         std::unordered_map<std::string_view, Bus*> busname_to_bus_;
-        std::unordered_map<std::string_view, std::unordered_set<std::string_view>> stop_and_stopping_buses_;
+        std::unordered_map<std::string_view, std::unordered_set<Bus*>> stop_and_stopping_buses_;
         std::unordered_map<std::pair<Stop*, Stop*>, int, StopsDistanceHasher> stops_distance_;
 
     public:
         void AddStop(std::tuple<std::string , double, double>& stop);
+        void AddStop(std::tuple<std::string , double, double>&& stop);
 
         Stop FindStop(std::string_view);
 
-        void AddBus(std::string& bus, std::vector<std::string>& stops);
+        void AddBus(std::string& bus, std::vector<std::string>& stops, bool is_roundtrip);
 
         void SetDistanceBetweenStops(std::tuple<std::string, int, std::string>& stop_distance_to_stop);
 
         Bus FindBus(std::string_view);
 
-        BusInfo GetBusInfo(std::string_view bus) const;
+        std::optional<BusInfo> GetBusInfo(std::string_view bus) const;
 
-        std::unordered_set<std::string_view> GetStopInfo(std::string_view stop) const;
+        const std::unordered_set<Bus*>* GetStopInfo(std::string_view stop) const;
+
+        std::vector<const Bus*> GetBuses() const;
+
+        std::vector<const Stop*> GetStopsIncludedInRoutes() const;
     };
 }
