2d78d91f9f8fcd4e4bfe7cad5b3be38cd5e08dc1
KlimchukKS <klinchuk1996@yandex.ru>
2023-08-18 01:35:17 +0500

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,17 +1,19 @@
 #include "transport_catalogue.h"
 #include "json_reader.h"
 #include "map_renderer.h"
-#include "request_handler.h"
 
 #include <iostream>
+#include <iomanip>
 
 int main() {
-
     transport_catalogue::TransportCatalogue db;
     renderer::MapRenderer mr;
 
     JsonReader json_reader(db, mr);
     json_reader.ParseJSON(std::cin);
+
+    std::cout << std::setprecision(6);
+    
     json_reader.OutStatRequests(std::cout);
 
     return 0;
