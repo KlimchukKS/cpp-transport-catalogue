4c392941249cae5dee4215c77bf5feed7217f5a3
KlimchukKS <klinchuk1996@yandex.ru>
2023-08-28 00:39:10 +0500

    Update serialization.h

--- transport-catalogue/serialization.h
+++ transport-catalogue/serialization.h
@@ -12,4 +12,12 @@
 
 void Serialize(const std::filesystem::path& path, serialization_data::SerializationData&& s_data);
 
+transport_catalogue_serialize::TransportCatalogue SerializeTransportCatalogue(serialization_data::SerializationData& s_data);
+transport_catalogue_serialize::VisualizationSettings SerializeVisualizationSettings(renderer::VisualizationSettings& vs_data);
+transport_catalogue_serialize::Color GetColor(svg::Color& col);
+
 serialization_data::SerializationData Deserialize(const std::filesystem::path& path);
+
+renderer::VisualizationSettings DeserializeVisualizationSettings(transport_catalogue_serialize::VisualizationSettings vs);
+serialization_data::SerializationData DeserializeTransportCatalogue(transport_catalogue_serialize::TransportCatalogue& tc);
+svg::Color DeserializeGetColor(transport_catalogue_serialize::Color& c);
