0a4914a19427db52c62ad598ed7386698024f2b8
KlimchukKS <klinchuk1996@yandex.ru>
2023-08-27 22:53:08 +0500

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,20 +1,47 @@
 #include "transport_catalogue.h"
+#include "serialization.h"
 #include "json_reader.h"
 #include "map_renderer.h"
 
+#include <fstream>
 #include <iostream>
-#include <iomanip>
+#include <string_view>
 
-int main() {
-    transport_catalogue::TransportCatalogue db;
-    renderer::MapRenderer mr;
+using namespace std::literals;
 
-    JsonReader json_reader(db, mr);
-    json_reader.ParseJSON(std::cin);
+void PrintUsage(std::ostream& stream = std::cerr) {
+    stream << "Usage: transport_catalogue [make_base|process_requests]\n"sv;
+}
 
-    std::cout << std::setprecision(6);
-    
-    json_reader.OutStatRequests(std::cout);
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        PrintUsage();
+        return 1;
+    }
 
-    return 0;
+    const std::string_view mode(argv[1]);
+
+    if (mode == "make_base"sv) {
+
+        transport_catalogue::TransportCatalogue db;
+        renderer::MapRenderer mr;
+
+        JsonReader json_reader(db, mr);
+
+        auto [serialization_setting, serialization_data] = json_reader.ParseJSONtoGetDataForSerialization(std::cin);
+        Serialize(serialization_setting,  std::move(serialization_data));
+
+    } else if (mode == "process_requests"sv) {
+
+        transport_catalogue::TransportCatalogue db;
+        renderer::MapRenderer mr;
+
+        JsonReader json_reader(db, mr);
+        json_reader.ParseJsonProcessRequests(std::cin);
+        json_reader.OutStatRequests(std::cout);
+
+    } else {
+        PrintUsage();
+        return 1;
+    }
 }
